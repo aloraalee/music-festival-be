773e6db1f9e2a09234b2fe3fd03449e31d00cec2
Alora Riley <alora.riley@gmail.com>
2025-01-21 13:20:11 -0500

    fix: Change item to show.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,5 @@
 class Show < ApplicationRecord
-  has_many :schedule_items
+  has_many :schedule_shows
   has_many :schedules, through: :schedule_shows
   validates :artist, presence: true
   validates :location, presence: true
