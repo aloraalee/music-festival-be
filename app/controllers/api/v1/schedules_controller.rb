f0f1172c1da52c45cfc5b3838ea48f91ca7ee6fb
Alora Riley <alora.riley@gmail.com>
2025-01-21 14:07:09 -0500

    feat: Pass get all schedules endpoint.

--- app/controllers/api/v1/schedules_controller.rb
+++ app/controllers/api/v1/schedules_controller.rb
@@ -1,3 +1,6 @@
-class Api::V1::SchedulessController < ApplicationController
-
+class Api::V1::SchedulesController < ApplicationController
+  def index
+    schedules = Schedule.all 
+    render json: ScheduleSerializer.new(schedules), status: :ok
+  end
 end
---
+++ spec/requests/schedule_request_spec.rb
@@ -0,0 +1,61 @@
+require "rails_helper"
+
+RSpec.describe "Schedules", type: :request do
+  before :each do
+    @user_1 = create(:user)
+    @user_2 = create(:user)
+    @user_3 = create(:user)
+
+    @schedule_1 = create(:schedule, user_id: @user_1.id)
+    @schedule_2 = create(:schedule, user_id: @user_1.id)
+    
+    @schedule_3 = create(:schedule, user_id: @user_2.id)
+
+    @schedule_4 = create(:schedule, user_id: @user_3.id)
+    @schedule_5= create(:schedule, user_id: @user_3.id)
+
+    @show_1 = create(:show)
+    @show_2 = create(:show)
+    @show_3 = create(:show)
+    @show_4 = create(:show)
+    @show_5 = create(:show)
+    @show_6 = create(:show)
+    @show_7 = create(:show)
+    @show_8 = create(:show)
+    @show_9 = create(:show)
+    @show_10 = create(:show)
+
+    @schedule_1.shows << @show_1
+    @schedule_1.shows << @show_2
+    @schedule_1.shows << @show_3
+
+    @schedule_2.shows << @show_9
+    @schedule_2.shows << @show_10
+    
+    @schedule_3.shows << @show_1
+    @schedule_3.shows << @show_2
+    @schedule_3.shows << @show_3
+
+    @schedule_4.shows << @show_4
+    @schedule_4.shows << @show_5
+    @schedule_4.shows << @show_6
+    @schedule_4.shows << @show_7
+    @schedule_4.shows << @show_8
+
+    @schedule_5.shows << @show_3
+  end
+
+  describe "Get All Schedules Endpoint" do
+    it "retrieves all schedules" do
+
+      get api_v1_schedules_path
+
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+
+      expect(json[:data].count).to eq(5)
+      expect(json[:data][0][:attributes]).to have_key(:title)
+      expect(json[:data][0][:attributes]).to have_key(:date)
+    end
+  end
+end
