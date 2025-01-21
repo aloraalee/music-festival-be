6e6255ccf695d16d011154b5845f420db2c2154d
Alora Riley <alora.riley@gmail.com>
2025-01-20 23:57:53 -0500

    fix: Change seed data to better match a admin view.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,6 +2,6 @@ class Schedule < ApplicationRecord
   belongs_to :user
   has_many :schedule_shows
   has_many :shows, through: :schedule_shows
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :date, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,21 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-user_1 = User.find_or_create_by!(first_name: "Alora", last_name: "Coolgirl", email: "bubblybubble@yahoo.com")
-user_2 = User.find_or_create_by!(first_name: "Zach", last_name: "Smoothguy", email: "spacemarines@hotmail.com")
-user_3 = User.find_or_create_by!(first_name: "Ellen", last_name: "Frizzle", email: "caligirl@gmail.com")
-user_4 = User.find_or_create_by!(first_name: "Brian", last_name: "Nowak", email: "spanishiscool@gmail.com")
+alora = User.find_or_create_by!(first_name: "Alora", last_name: "Coolgirl", email: "bubblybubble@yahoo.com")
+zach = User.find_or_create_by!(first_name: "Zach", last_name: "Smoothguy", email: "spacemarines@hotmail.com")
+ellen = User.find_or_create_by!(first_name: "Ellen", last_name: "Frizzle", email: "caligirl@gmail.com")
+brian = User.find_or_create_by!(first_name: "Brian", last_name: "Nowak", email: "spanishiscool@gmail.com")
 
-schedule_1 = Schedule.find_or_create_by!(title: "Thursday", date: "2023-06-08", user_id: user_1.id)
-schedule_2 = Schedule.find_or_create_by!(title: "Friday", date: "2023-06-09", user_id: user_1.id)
-schedule_3 = Schedule.find_or_create_by!(title: "Saturday", date: "2023-06-10", user_id: user_1.id)
+alora_schedule_1 = Schedule.find_or_create_by!(title: "Alora's Thursday Schedule", date: "2023-06-08", user_id: alora.id)
+alora_schedule_3 = Schedule.find_or_create_by!(title: "Alora's Saturday Schedule", date: "2023-06-10", user_id: alora.id)
+
+zach_schedule_1 = Schedule.find_or_create_by!(title: "Zach's Thursday Schedule", date: "2023-06-08", user_id: zach.id)
+zach_schedule_2 = Schedule.find_or_create_by!(title: "Zach's Saturday Schedule", date: "2023-06-10", user_id: zach.id)
+
+ellen_schedule_1 = Schedule.find_or_create_by!(title: "Ellen's Indie Pop/Rock Schedule", date: "2023-06-10", user_id: ellen.id)
+
+brian_schedule_1 = Schedule.find_or_create_by!(title: "Brian's Must See Artist", date: "2023-06-09", user_id: brian.id)
+brian_schedule_2 = Schedule.find_or_create_by!(title: "Brian's Hip Hop", date: "2023-06-08", user_id: brian.id)
 
 show_1 = Show.find_or_create_by!(artist: "Baby Keem", location: "Porto Main Stage", show_time: "2023-06-08 21:00")
 show_2 = Show.find_or_create_by!(artist: "Baby Keem", location: "Porto Main Stage", show_time: "2023-06-08 21:00")
